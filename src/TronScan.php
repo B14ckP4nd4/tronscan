3c4be329783a4496e0379072f8c5bf16ee64fc83
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-01-13 19:17:41 +0000

    set properties and constructor

--- src/TronScan.php
+++ src/TronScan.php
@@ -4,4 +4,16 @@
 namespace BlackPanda\TronScan;
 class TronScan
 {
+    /*
+     * url of the tronScan API
+     */
+    private $api_url = 'https://apilist.tronscan.org/api/';
+
+    private $api;
+
+    public function __construct()
+    {
+        $this->api = new Client(['base_uri' => $this->api_url]);
+    }
+
 }
