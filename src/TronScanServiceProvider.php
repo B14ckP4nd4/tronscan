9679333eeee3248b2a6ba046f9ea02b99a38d2da
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-01-19 19:32:05 +0000

    bind the facade

--- src/TronScanServiceProvider.php
+++ src/TronScanServiceProvider.php
@@ -5,8 +5,16 @@ namespace BlackPanda\TronScan;
 
 
 use Carbon\Laravel\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class TronScanServiceProvider extends ServiceProvider
 {
+
+    public function register()
+    {
+        $this->app->bind('TronScan', function () {
+            return new TronScan; //Add the proper namespace at the top
+        });
+    }
 
 }
